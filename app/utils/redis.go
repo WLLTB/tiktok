526a0879ed12099766957384845567386fe153d7
WLLTB <1967475093@qq.com>
2023-01-14 13:28:23 +0800

    配置初始化与重构

--- app/utils/redis.go
+++ app/utils/redis.go
@@ -2,26 +2,42 @@ package utils
 
 import (
 	"fmt"
+	"log"
+	"time"
+
 	"github.com/go-redis/redis"
 )
 
-var client = redis.NewClient(&redis.Options{
-	Addr:     "localhost:6379",
-	Password: "",
-	DB:       0,
-})
+var client *redis.Client
 
-func GetValue(key string) string {
-	value, err := client.Get(key).Result()
+// InitClient 初始化 Redis 客户端
+func InitClient() {
+	client = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	_, err := client.Ping().Result()
 	if err != nil {
-		fmt.Println(err)
+		log.Panic("连接 Redis 失败: %w", err)
 	}
-	return value
+	log.Print("连接 Redis 成功")
 }
 
-func SetValue(key string, value string) {
-	err := client.Set(key, value, 0).Err()
+// GetValue 获取 Redis 中的值
+func GetValue(key string) (string, error) {
+	value, err := client.Get(key).Result()
 	if err != nil {
-		fmt.Println(err)
+		return "", fmt.Errorf("获取 key %s 失败: %w", key, err)
 	}
+	return value, nil
+}
+
+// SetValue 设置 Redis 中的值
+func SetValue(key string, value string, expire time.Duration) error {
+	err := client.Set(key, value, expire).Err()
+	if err != nil {
+		return fmt.Errorf("保存 key %s 失败: %w", key, err)
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	config "tiktok/app/config"
+	"tiktok/app/utils"
 )
 
 func main() {
 	r := gin.Default()
-
+	utils.InitClient()
 	config.InitGormDb()
 	config.InitRouter(r)
 
