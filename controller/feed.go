76b9517cebc61fcfadf1ac3491e5e84e9a483008
WLLTB <1967475093@qq.com>
2023-01-14 12:03:53 +0800

    配置项目基础结构

--- controller/feed.go
+++ controller/feed.go
@@ -12,6 +12,7 @@ type FeedResponse struct {
 	NextTime  int64   `json:"next_time,omitempty"`
 }
 
+// Feed same demo video list for every request
 func Feed(c *gin.Context) {
 	c.JSON(http.StatusOK, FeedResponse{
 		Response:  Response{StatusCode: 0},
