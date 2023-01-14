5b03c9a129a4bd6b10d48dfc351e77e263e03925
WLLTB <1967475093@qq.com>
2023-01-14 16:04:14 +0800

    修改日志格式

--- app/config/log.go
+++ app/config/log.go
@@ -2,10 +2,8 @@ package config
 
 import (
 	"log"
-	"time"
 )
 
 func InitLog() {
-	log.SetFlags(log.Ltime)
-	log.SetPrefix("[GIN_LOG] " + time.Now().Format("2006/01/02 - 15:04:05") + " | ")
+	log.SetPrefix("[GIN_LOG] ")
 }
