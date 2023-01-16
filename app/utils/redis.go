589ec697b7fb33542e2fe81d98c9d846bb3de4c4
WLLTB <1967475093@qq.com>
2023-01-16 20:19:11 +0800

    补充 Redis 操作set 的工具方法

--- app/utils/redis.go
+++ app/utils/redis.go
@@ -26,3 +26,31 @@ func SetValue(key string, value string, expire time.Duration) error {
 	}
 	return nil
 }
+
+// PushSet 向 Set 中添加元素
+func PushSet(key string, value string) error {
+	err := RedisClient.SAdd(key, value).Err()
+	if err != nil {
+		return fmt.Errorf("在 %s 集合中保存 %s 失败: %w", key, value, err)
+	}
+	return nil
+}
+
+// GetSet 获取 Set 中的所有元素
+func GetSet(key string) ([]string, error) {
+	var set []string
+	set, err := RedisClient.SMembers(key).Result()
+	if err != nil {
+		return set, fmt.Errorf("获取 %s 集合失败: %w", key, err)
+	}
+	return set, nil
+}
+
+// HasSetValue 判断set中是否有某个值
+func HasSetValue(key, value string) (bool, error) {
+	has, err := RedisClient.SIsMember(key, value).Result()
+	if err != nil {
+		return false, fmt.Errorf("判断 key %s 中是否有 %s 失败: %w", key, value, err)
+	}
+	return has, nil
+}
