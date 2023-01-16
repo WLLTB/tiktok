3dc037d5a12be9ec7440c46fe07a755fc399a93c
llx <lianglinxia2022@163.com>
2023-01-16 21:42:25 +0800

    用户信息接口v2

--- app/controller/user.go
+++ app/controller/user.go
@@ -47,16 +47,15 @@ func Login(c *gin.Context) {
 func UserInfo(c *gin.Context) {
 	token := c.Query("token")
 	tokenClaim, _ := utils.VerifyToken(token)
-	currentUserId := tokenClaim["userId"].(string)
+	userId :=  tokenClaim["userId"]
+	currentUserId := fmt.Sprintf("%v", userId)
 	targetUserId := c.Query("user_id")
 
+	// 类型转换
 	currentUserIdInt, _ := strconv.ParseInt(currentUserId, 10, 64)
 	targetUserIdInt, _ := strconv.ParseInt(targetUserId, 10, 64)
-	fmt.Println(currentUserIdInt)
-	fmt.Println(targetUserIdInt)
 
-	//userInfo := service.SupplementTargetUserInfo(currentUserIdInt, targetUserIdInt)
-	userInfo := service.SupplementTargetUserInfo(1, 1)
+	userInfo := service.SupplementTargetUserInfo(currentUserIdInt, targetUserIdInt)
 	c.JSON(http.StatusOK, UserInfoResponse{
 		Response: Response{StatusCode: 0},
 		User:     userInfo,
