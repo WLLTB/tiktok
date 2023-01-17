acf93ab66160f2ec43589c3c6ec4dbadf3b56fae
llx <lianglinxia2022@163.com>
2023-01-18 00:28:50 +0800

    点赞操作 接口

--- app/controller/favorite.go
+++ app/controller/favorite.go
@@ -3,17 +3,45 @@ package controller
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
+	"tiktok/app/config"
+	"tiktok/app/schema"
+	"tiktok/app/utils"
 	. "tiktok/app/vo"
 )
 
 // FavoriteAction no practical effect, just check if token is valid
 func FavoriteAction(c *gin.Context) {
-	token := c.Query("token")
+	videoId := c.PostForm("video_id")
+	videoIdInt, _ := strconv.ParseInt(videoId, 10, 64)
+	actionType := c.PostForm("action_type")
+	actionTypeInt, _ := strconv.ParseInt(actionType, 10, 64)
+	token := c.PostForm("token")
+	userId, _ := utils.VerifyToken(token)
 
-	if _, exist := usersLoginInfo[token]; exist {
-		c.JSON(http.StatusOK, Response{StatusCode: 0})
-	} else {
-		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
+	// 判断video是否存在
+	videoOne := schema.Video{}
+	config.Db.Model(&schema.Video{}).Select("Id").Where("Id = ?", videoIdInt).Find(&videoOne)
+
+	if videoOne.Id == 0 {
+		c.JSON(http.StatusOK, Response{
+			StatusCode: 0,
+			StatusMsg:  "Video is not exist",
+		})
+	} else if actionTypeInt == 1 {
+		likeOne := schema.Like{UserId: userId, VideoId: videoIdInt}
+		config.Db.Model(&schema.Like{}).Create(&likeOne)
+		c.JSON(http.StatusOK, Response{
+			StatusCode: 0,
+			StatusMsg:  "Like successful",
+		})
+	} else if actionTypeInt == 2 {
+		likeOne := schema.Like{UserId: userId, VideoId: videoIdInt}
+		config.Db.Model(&schema.Like{}).Where(likeOne).Delete(&likeOne)
+		c.JSON(http.StatusOK, Response{
+			StatusCode: 0,
+			StatusMsg:  "Unlike successful",
+		})
 	}
 }
 
