3a61d0939a85574283b17aaf8ca7724aa2c0fa0a
WLLTB <1967475093@qq.com>
2023-01-18 19:19:07 +0800

    字段名常量

---
+++ app/constant/field.go
@@ -0,0 +1,16 @@
+package constant
+
+const ID = "id"
+const UserId = "userId"
+const VideoID = "video_id"
+const CommentText = "comment_text"
+const CommentCreateDate = "create_date"
+const FollowerId = "follower_id"
+const LikeDate = "like_date"
+const UserName = "username"
+const PassWord = "password"
+const AuthorId = "author_id"
+const PlayURL = "play_url"
+const CoverURL = "cover_url"
+const PublishTime = "publish_time"
+const Title = "title"
